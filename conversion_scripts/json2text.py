6399c524c1c9b43c09351709b21152e6ea0cff94
Matt Post <mattpost@microsoft.com>
2021-12-19 03:30:00 +0000

    added length accounting with spm

--- conversion_scripts/json2text.py
+++ conversion_scripts/json2text.py
@@ -15,14 +15,19 @@ def noWS(s):
     return re.sub(r"\s\t\n", "", s)
 
 
-def get_context(length, lineno, doc):
+def get_context(is_too_long, lineno, doc):
     context_line = lineno - 1
 
     source_context = []
     target_context = []
-    while context_line >= 1 and len(" ".join(source_context).split()) <= length:
+    while context_line >= 1:
         source_context.insert(0, doc[0][context_line-1])
         target_context.insert(0, doc[1][context_line-1])
+        if is_too_long(source_context):
+            source_context.pop(0)
+            target_context.pop(0)
+            break
+
         context_line -= 1
 
     return source_context, target_context
@@ -36,6 +41,11 @@ def stripread(fh):
 
 
 def main(args):
+    spm = None
+    if args.spm:
+        from sentencepiece import SentencePieceProcessor
+        spm = SentencePieceProcessor(model_file=args.spm)
+
     filenames = {}
     for subfolder in os.listdir(args.dir):
         for file_name in os.listdir(os.path.join(args.dir, subfolder)):
@@ -67,52 +77,21 @@ def main(args):
             print(f"Fatal: bad source", file=sys.stderr)
             sys.exit(1)
 
-        source_context, target_context = get_context(args.max_length, lineno, filenames[filename])
+        def is_too_long(context):
+            if spm:
+                return sum([len(x) for x in spm.encode(context + [source])]) > args.max_length
+            else:
+                return sum([len(x.split()) for x in context + [source]]) > args.max_length
+
+        source_context, target_context = get_context(is_too_long, lineno, filenames[filename])
         source_line = args.separator.join(source_context + [source])
         target_line = args.separator.join(target_context + [target])
         print(source_line, target_line, sep="\t")
 
-        # for error in sentence["errors"]:
-        #     contrastive = error["contrastive"]
-        #     print(context + [contrastive], sep=args.separator)
-
-# sub printContext{
-#     my $context = $_[0];
-#     my $line = $_[1];
-#     my $filename = $_[2];
-#     my $filenames = $_[3];
-#     my $json_out_s = $_[4];
-#     my $json_out_t = $_[5];
-
-#     my %json_context_s =();
-#     my %json_context_t =();
-#     my $i=1;
-
-#     for(my $c=$context;$c>=1;$c--){
-#         my $src_line ="\n";
-#         my $trg_line = "\n";
-#         if($line-$c >= 1){
-#                 $src_line = @{$filenames->{$filename}{"src"}}[$line-$c];
-#                 $trg_line = @{$filenames->{$filename}{"trg"}}[$line-$c]; 
-#         }
-#         if($context_json){
-#                 $src_line =~ s/\n//;
-#                 $trg_line =~ s/\n//;
-#                 $json_context_s{$i} = $src_line;
-#                 $json_context_t{$i} = $trg_line;
-#                 $i++;
-#         }
-#         else{
-#                 print OUT_S_CONTEXT $src_line;
-#                 print OUT_T_CONTEXT $trg_line;
-#        }
-#         if($context_json){
-#                 push(@{$json_out_s}, \%json_context_s );
-#                 push(@{$json_out_t}, \%json_context_t );
-#         }
-#     }
-
-# }
+        for error in sentence["errors"]:
+            contrastive = error["contrastive"]
+            target_line = args.separator.join(target_context + [contrastive])
+            print(source_line, target_line, sep="\t")
 
 
 if __name__ == "__main__":
@@ -123,6 +102,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dir", default="documents")
     parser.add_argument("-m", "--max-length", type=int, default=250)
     parser.add_argument("--separator", default=" <eos> ")
+    parser.add_argument("--spm")
     parser.add_argument("json_file")
     args = parser.parse_args()
 
