79637fefbd581a132e500a2c7e98cb6c2754cbaa
Matt Post <mattpost@microsoft.com>
2022-05-30 17:11:07 +0000

    added distance range, all pronoun

--- bin/evaluate_tsv.py
+++ bin/evaluate_tsv.py
@@ -12,18 +12,21 @@ def main(args):
     total = 0
     for lineno, line in enumerate(sys.stdin, 1):
         dist, label, pronoun, source, reference, system = line.rstrip().split("\t")
+        pronoun = pronoun.lower()
 
         if label != "correct":
             continue
-        if args.distance is not None and args.distance != int(dist):
+
+        if int(dist) < args.distance[0] or int(dist) > args.distance[1]:
             continue
-        if args.pronoun and pronoun not in args.pronoun:
+
+        if args.pronouns and "all" not in args.pronouns and pronoun not in args.pronouns:
             continue
 
         output = tok.tokenize(system.split(args.separator)[-1], return_str=True)
 
         total += 1
-        if pronoun.lower() in output.lower().split():
+        if pronoun in output.lower().split():
             if args.debug:
                 print(lineno, pronoun, "in", output, file=sys.stderr)
             correct += 1
@@ -39,8 +42,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
     parser.add_argument("--separator", default=" <eos> ")
-    parser.add_argument("--distance", "-d", type=int)
-    parser.add_argument("--pronoun", "-p", nargs="+", default=[], choices="er es sie".split())
+    parser.add_argument("--distance", "-d", nargs=2, default=[0, 100], type=int)
+    parser.add_argument("--pronouns", "-p", nargs="+", default=[], choices="all er es sie".split())
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
 
